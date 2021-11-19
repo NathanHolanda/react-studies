3a2ec0b2d4b861af13b5df2f26db846fb737beb1
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2021-11-19 17:34:49 -0300

    exercício com useCallback

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import Buttons from './UseCallbackButtons'
 
 const UseCallback = (props) => {
+    const[value, setValue] = useState(0)
+
+    const inc = useCallback((num) => setValue(current => current + num), [setValue])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <SectionTitle title="Exercício #1" />
+            <section className="center">
+                <span className="text">{value}</span>
+                <Buttons inc={inc}></Buttons>
+            </section>
         </div>
     )
 }
