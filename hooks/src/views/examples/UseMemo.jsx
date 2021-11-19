be9f4f58b4f70600a46d17aa8daa32346b0d8d7b
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2021-11-19 16:16:58 -0300

    exercício com useMemo

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, {useState, useMemo} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseMemo = (props) => {
+    const [value1, setValue1] = useState(0)
+    const [value2, setValue2] = useState(0)
+    const [value3, setValue3] = useState(0)
+
+    const sum = (a, b) => {
+        const future =  Date.now() + 2000
+        while(Date.now() < future)
+            continue
+        return a + b
+    }
+
+    const result = useMemo(() => sum(value1, value2), [value1, value2])
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
                 subtitle="Retorna um valor memoizado!"
             />
+            <SectionTitle title="Exercício #1" />
+            <section className="center">
+                <input type="number" className="input" value={value1} onChange={e => setValue1(+e.target.value)} />
+                <input type="number" className="input" value={value2} onChange={e => setValue2(+e.target.value)} />
+                <input type="number" className="input" value={value3} onChange={e => setValue3(+e.target.value)} />
+                <span className="text">Resultado: {result}</span>
+            </section>
         </div>
     )
 }
